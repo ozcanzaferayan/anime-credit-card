94a17252ce6932795183f0e387e4358a8c62f273
Zafer AYAN <ozcanzaferayan@gmail.com>
2023-04-22 16:02:00 +0300

    Change base

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/ozcanzaferayan/anime-credit-card",
+  base: "/anime-credit-card/",
 });
